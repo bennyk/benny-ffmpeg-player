115f59c2cf6c8537005576ecddb80a5f47b470eb
Benny Khoo <benny_khoo_99@yahoo.com>
2015-02-27 16:46:44 +0800

    scan sdcard for media files on pull to refresh gesture.

--- MyFFmpegPlayer/app/src/main/java/com/bennykhoo/ffmpeg/myffmpegplayer/MainActivity.java
+++ MyFFmpegPlayer/app/src/main/java/com/bennykhoo/ffmpeg/myffmpegplayer/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.widget.CursorAdapter;
@@ -20,6 +21,13 @@ import android.widget.ListView;
 import com.bennykhoo.ffmpeg.myffmpegplayer.adapter.MainAdapter;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
+
+import eu.erikw.PullToRefreshListView;
 
 public class MainActivity extends ActionBarActivity implements OnItemClickListener {
 
@@ -29,7 +37,7 @@ public class MainActivity extends ActionBarActivity implements OnItemClickListen
 
     private static final int PICKFILE_RESULT_CODE = 123;
 
-    private ListView mListView;
+    private PullToRefreshListView mListView;
 	private CursorAdapter mAdapter;
 
     private VideoListSQLiteHelper dbHelper;
@@ -37,6 +45,23 @@ public class MainActivity extends ActionBarActivity implements OnItemClickListen
             VideoListSQLiteHelper.COLUMN_TITLE,
             VideoListSQLiteHelper.COLUMN_URL,
             VideoListSQLiteHelper.COLUMN_ENC_KEY };
+
+    // Exhaustive listing of video file extension according to http://en.wikipedia.org/wiki/Video_file_format
+    private static final String[] mediaFileExtensionsArray = new String[] { ".webm", ".mkv", ".flv",
+            // TODO ogg file doesn't play now hmmm...
+            //".ogv", ".ogg", ".ogv",
+            ".drc", ".mng", ".avi", ".mov", ".qt", ".wmv", ".yuv", ".rm", ".rmvb", ".asf", ".mp4", ".m4p", ".m4v", ".mpg", ".mp2", ".mpeg", ".mpe", ".mpv", ".m2v", ".m4v", ".svi", ".3gp", ".3g2", ".mxf", ".roq", ".nsv"
+    };
+    private static Set<String> mediaFileExtensionsHash = new HashSet<String>(Arrays.asList(mediaFileExtensionsArray));
+
+    private static String getLastPathFragment(String fname) {
+        String result = fname;
+        int lastIndex = fname.lastIndexOf("/");
+        if (lastIndex >= 0) {
+            result = fname.substring(lastIndex + 1);
+        }
+        return result;
+    }
 
     @Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -46,9 +71,77 @@ public class MainActivity extends ActionBarActivity implements OnItemClickListen
         dbHelper = new VideoListSQLiteHelper(this);
         mAdapter = new MainAdapter(this);
 
-		mListView = (ListView) findViewById(android.R.id.list);
+		mListView = (PullToRefreshListView) findViewById(android.R.id.list);
 		mListView.setAdapter(mAdapter);
 		mListView.setOnItemClickListener(this);
+        mListView.setTextRefreshing("Scanning for media files...");
+//        mListView.setRefreshing();
+
+        mListView.setOnRefreshListener(new PullToRefreshListView.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Log.w(TAG, "refreshing");
+                AsyncTask<File, Void, Void> scanTask = new AsyncTask<File, Void, Void>() {
+
+                    void startScanning(File startDir) {
+                        LinkedList<File> q = new LinkedList<>();
+
+                        q.add(startDir);
+
+                        while (q.size() > 0) {
+                            File current = q.removeFirst();
+                            File[] list = current.listFiles();
+
+                            for (File f : list) {
+                                if (f.isDirectory()) {
+//                                    Log.d("", "Dir: " + f.getAbsoluteFile());
+                                    q.add(f);
+                                } else {
+                                    String filename = f.toString();
+
+//                                    Log.d("", "file: " + filename);
+                                    int lastIndex = filename.lastIndexOf(".");
+                                    if (lastIndex >= 0) {
+                                        String ext = filename.substring(lastIndex);
+                                        if (mediaFileExtensionsHash.contains(ext)) {
+                                            Log.d("", "found a media file: " + f.getAbsoluteFile());
+
+                                            // add an entry to our cache database.
+                                            addURL(getLastPathFragment(filename), filename, null);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                    }
+
+                    @Override
+                    protected void onPreExecute() {
+                        super.onPreExecute();
+                        clearAll();
+                    }
+
+                    @Override
+                    protected Void doInBackground(File... files) {
+                        for (int i = 0; i < files.length; i++) {
+                            startScanning(files[i]);
+                        }
+
+                        return null;
+                    }
+
+                    @Override
+                    protected void onPostExecute(Void aVoid) {
+                        super.onPostExecute(aVoid);
+                        refresh();
+                        mListView.onRefreshComplete();
+                    }
+                };
+
+                scanTask.execute(Environment.getExternalStorageDirectory());
+            }
+        });
 
         refresh();
     }
@@ -131,6 +224,11 @@ public class MainActivity extends ActionBarActivity implements OnItemClickListen
         mAdapter.swapCursor(cursor);
     }
 
+    void clearAll() {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        db.delete(VideoListSQLiteHelper.TABLE_VIDEOS, "", null);
+    }
+
     void startPickFileActivity() {
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
         intent.setType("file/*");
@@ -142,4 +240,5 @@ public class MainActivity extends ActionBarActivity implements OnItemClickListen
         intent.putExtra(AppConstants.VIDEO_PLAY_ACTION_EXTRA_URL, Uri.decode(uri.toString()));
         startActivity(intent);
     }
+
 }
